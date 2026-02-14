31af5c11324419fee4735a49ccb578c3c93b2826
Yevhen Pastukhov <yevhen.pastukhov@newpage.software>
2026-02-14 11:17:18 +0200

    Remove gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: "/matrix-app",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
