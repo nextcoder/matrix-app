5f1429c5165b27b111296e45232a12f74c04f37b
Yevhen Pastukhov <yevhen.pastukhov@newpage.software>
2026-02-14 11:03:41 +0200

    Updates

--- _next/static/kF5jnSQBhLq3eodr0usUA/_buildManifest.js
+++ _next/static/kF5jnSQBhLq3eodr0usUA/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/matrix-app//_next/:path+",
-        "destination": "/matrix-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
